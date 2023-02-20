8585771e8243266c77720c3cee7afc5b7ba5b239
thejyotipatel <jyotipatelfyit77100@gmail.com.com>
2023-02-20 17:04:59 +0530

    update route

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -8,15 +8,15 @@ import {
 } from '../controllers/hotels.js'
 const router = express.Router()
 
+// GET
+router.get('/:id', getHotel)
+// GET ALL
+router.get('/', getAllHotels)
 // CREATE
 router.post('/', createHotel)
 // UPDATE
 router.put('/:id', updateHotel)
 // DELETE
 router.delete('/:id', deleteHotel)
-// GET
-router.get('/:id', getHotel)
-// GET ALL
-router.get('/', getAllHotels)
 
 export default router
