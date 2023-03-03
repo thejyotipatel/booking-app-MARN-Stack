e488010238ab5b3b3e68ddfeb220bdfc2ccb821a
thejyotipatel <thejyotipatel>
2023-03-03 14:33:26 +0530

    updared

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Route, 
+  RouterProvider
+} from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+// layouts
+import RootLayout from "./layouts/RootLayout"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+)
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ client/src/layouts/RootLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet, NavLink } from "react-router-dom"
+
+export default function RootLayout() {
+  return (
+    <div className="root-layout container">
+      <header>
+        <nav>
+          <h1>Booking.red</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+          <NavLink to="help">Help</NavLink>
+          <NavLink to="careers">Careers</NavLink>
+        </nav>
+        {/* <Breadcrumbs /> */}
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
